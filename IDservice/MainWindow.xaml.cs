55c8817748ad565768d6fa0cc3163fc02162c177
kib357 <kib357@gmail.com>
2012-12-01 22:42:09 +0600

    some references added

---
+++ IDservice/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Printing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IDservice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var viewModel = new IdViewModel();
+            this.DataContext = viewModel;
+
+            
+        }
+
+        private void Print_OnClick(object sender, RoutedEventArgs e)
+        {
+            var queue = LocalPrintServer.GetDefaultPrintQueue();
+            queue.CurrentJobSettings.Description = "idservice";
+            var writer = PrintQueue.CreateXpsDocumentWriter(queue);
+            writer.Write(Area);
+            //var pr = new PrintDialog();
+            //if (pr.ShowDialog() == true)
+            //{
+            //    pr.PrintVisual(Area, "grid");
+            //}
+        }
+    }
+}
